3f351c39dd360468bb67fa61f6cf29101f0d59e3
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-04-28 14:37:44 -0400

    Add rand and reference to platter

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use std::sync::{Arc, RwLock};
+
+use rand::prelude::*;
+
 pub mod zort;
 
 pub struct Gene<T> {
@@ -7,25 +11,28 @@ pub struct Gene<T> {
 }
 
 pub struct Genome<T> {
+    platter: Arc<RwLock<Platter>>,
     genes: Vec<Gene<T>>,
 }
 
 pub struct Platter {
     innovation: usize,
+    rng: ThreadRng,
 }
 
 type Bias<T> = fn(Gene<T>, Gene<T>) -> Gene<T>;
 
 impl<T> Genome<T> {
-    pub fn new() -> Self { Self { genes: vec![] } }
+    pub fn new(platter: Arc<RwLock<Platter>>) -> Self { Self { platter, genes: vec![] } }
 
     pub fn splice(
+        platter: Arc<RwLock<Platter>>,
         left: &Self,
         right: &Self,
         bias: Bias<T>,
         mutation: f64
     ) -> Self {
-        let mut genome = Genome::new();
+        let mut genome = Genome::new(platter);
         let zort = zort::Zort::new(
             left.genes.iter(),
             right.genes.iter(),
@@ -45,6 +52,7 @@ impl<T> Genome<T> {
     }
 
     pub fn push(&mut self, gene: &Gene<T>, mutation: f64) {
+        let y: f64 = self.platter.write().unwrap().rng.gen();
     }
 
     pub fn collect(self) -> Vec<T> {
@@ -59,5 +67,9 @@ impl<T> Genome<T> {
 }
 
 impl Platter {
-    pub fn new() -> Self { Self { innovation: 0 } }
+    pub fn new() -> Self { Self { innovation: 0, rng: rand::thread_rng() } }
+
+    pub fn rand(mut self) -> f64 {
+        self.rng.gen()
+    }
 }
